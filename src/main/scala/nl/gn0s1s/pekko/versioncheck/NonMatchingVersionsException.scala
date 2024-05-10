6056c50b2a92eda944ae34b0cfc6b2b50c51da5f
philippus <philippus@gmail.com>
2024-05-10 20:44:11 +0200

    Mention "Pekko module" in exception message

--- src/main/scala/nl/gn0s1s/pekko/versioncheck/NonMatchingVersionsException.scala
+++ src/main/scala/nl/gn0s1s/pekko/versioncheck/NonMatchingVersionsException.scala
@@ -1,3 +1,3 @@
 package nl.gn0s1s.pekko.versioncheck
 
-case object NonMatchingVersionsException extends IllegalStateException("Non-matching versions found")
+case object NonMatchingVersionsException extends IllegalStateException("Non-matching Pekko module versions found")
